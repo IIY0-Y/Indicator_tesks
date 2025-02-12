ff8772d510831455bbb1355c536cfa84f8b661af
MoonFlowww <161968556+MoonFlowww@users.noreply.github.com>
2025-02-12 12:04:37 +0100

    Create HT_TRENDLINE.py

---
+++ Indicator tesks/Overlap Studies/HT_TRENDLINE.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import talib as ta
+
+def ht_trendline(self, df, col, coin_symbol, interval, is_price):
+    if is_price:
+        ht_trendline_col_name = coin_symbol + "_" + interval + "_ht_trendline"
+    else:
+        ht_trendline_col_name = col + "_ht_trendline"
+    ht_trendline = ta.HT_TRENDLINE(df[col])
+    df[ht_trendline_col_name] = ht_trendline
+    df=df.dropna()
+    return df
