c46324cf0bd553a4af297680891d18ac18b8b5f4
MoonFlowww <161968556+MoonFlowww@users.noreply.github.com>
2025-02-14 14:57:33 +0100

    Add files via upload

--- Indicator tesks/Indicators/Math_Transform/acos.py
+++ Indicator tesks/Indicators/Math_Transform/acos.py
@@ -1,5 +1,15 @@
 import pandas as pd
 import talib as ta
+CONFIG = {
+    "indicator_class": "Math Transform",
+    "indicator_name": "ACOS",
+    "indicator_payload": "",
+    "indicator_input_col": "real",
+    "indicator_return_col": "real",
+    "chart_area": "overlays",
+    "chart_type": "line",
+    "ind_type": "1"
+}
 
 def acos(df, col, coin_symbol, interval, is_price):
     if is_price:
--- Indicator tesks/Indicators/Math_Transform/ceil.py
+++ Indicator tesks/Indicators/Math_Transform/ceil.py
@@ -1,5 +1,15 @@
 import pandas as pd
 import talib as ta
+CONFIG = {
+    "indicator_class": "Math Transform",
+    "indicator_name": "CEIL",
+    "indicator_payload": "",
+    "indicator_input_col": "real",
+    "indicator_return_col": "real",
+    "chart_area": "overlays",
+    "chart_type": "line",
+    "ind_type": "1"
+}
 
 def ceil(df, col, coin_symbol, interval, is_price):
     if is_price:
