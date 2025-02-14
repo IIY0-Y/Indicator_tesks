f831bf657f2ee5d84922281d7a969389470ce2ff
MoonFlowww <161968556+MoonFlowww@users.noreply.github.com>
2025-02-14 15:00:41 +0100

    Add files via upload

--- Indicator tesks/Indicators/Statistic_Indicators/linearreg.py
+++ Indicator tesks/Indicators/Statistic_Indicators/linearreg.py
@@ -1,5 +1,15 @@
 import pandas as pd
 import talib as ta
+CONFIG = {
+    "indicator_class": "Statistic Functions",
+    "indicator_name": "LINEARREG",
+    "indicator_payload": "'timeperiod': 14",
+    "indicator_input_col": "real",
+    "indicator_return_col": "real",
+    "chart_area": "overlays",
+    "chart_type": "line",
+    "ind_type": "11"
+}
 
 def linearreg(df, col, coin_symbol, interval, is_price, timeperiod=14):
     if is_price:
--- Indicator tesks/Indicators/Statistic_Indicators/var.py
+++ Indicator tesks/Indicators/Statistic_Indicators/var.py
@@ -1,5 +1,18 @@
 import pandas as pd
 import talib as ta
+CONFIG = {
+    "indicator_class": "Statistic Functions",
+    "indicator_name": "VAR",
+    "indicator_payload": {
+        "timeperiod": 5,
+        "nbdev": 1
+    },
+    "indicator_input_col": "real",
+    "indicator_return_col": "real",
+    "chart_area": "Oscillators",
+    "chart_type": "line",
+    "ind_type": "n1"
+}
 
 def var(df, col, coin_symbol, interval, is_price, timeperiod=5, nbdev=1):
     if is_price:
